f52be7fda85415a27eedfdc3dfa074580d928e89
SusanaCahuantico <scahuantico@gmail.com>
2019-11-12 09:12:07 -0500

    leyendo data pokedex

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ const container = document.getElementById('container');
 
 const dataJSON = () => {
     // fetch('https://raw.githubusercontent.com/SusanaCahuantico/LIM009-DL-2.0/master/src/data/potter.json')
-    fetch('https://pokeapi.co/docs/v2.html/#pokemon-section')
+    fetch('https://pokeapi.co/api/v2/pokemon')
       .then(res => res.json())
-      .then(datos => {
-        console.log(datos);
-        dataJSON()
-      });
-  };
-//  dataJSON();
+      .then(data => {
+          console.log(data)
+      })
+      };
+  dataJSON();
